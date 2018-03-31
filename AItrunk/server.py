8f6c23e33313778815ad185ba8a5dce9b4a1799d
張景雲 <z5254215560@gmail.com>
2018-04-01 04:36:34 +0800

    aaaaa

---
+++ AItrunk/server.py
@@ -0,0 +1,26 @@
+# The core algorithm writing here
+from modules.camera import Camera
+from modules.controller import Controller
+from modules.DD import DD
+import time
+
+
+
+
+
+if __name__ == '__main__':
+    controller = Controller()
+    D1 = DD(27)    #Front DD , obstacle detection
+    D2 = DD(4)    ##Back DD , person detection
+    while(True):
+            if(D1.status() == False):
+                    # Obstacle has been detected
+                    # stop
+                    controller.Stop()
+            elif(D2.status() == True):
+                    # did not detect person
+                    # stop
+                    controller.Stop()
+            else:
+                    # Fron got no obstacle and do detect the face and the person is in the range
+                    controller.Forward()
