4fec1b52e8d87f792d510eee881104687d35f1c4
張景雲 <z5254215560@gmail.com>
2018-03-31 19:38:03 +0800

    test troll.py

--- AItrunk/troll.py
+++ AItrunk/troll.py
@@ -1,4 +1,10 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-print GPIO(4)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(7, GPIO.RISING)
+def my_callback():
+        print GPIO.input(7)
+        print "Gaga"
+GPIO.add_event_callback(7, my_callback)
+
+
