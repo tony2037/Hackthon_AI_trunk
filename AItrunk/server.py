1815576c723c63ec19f9dd853eeb7cc913170acb
tony2037 <ztex030640417@gmail.com>
2018-04-01 02:24:01 +0800

    Has construct the archeivement of core algorithm. Ztex is exhausted...

--- AItrunk/server.py
+++ AItrunk/server.py
@@ -17,3 +17,18 @@ if __name__ == '__main__':
 	D1 = DD(27)	#Front DD , obstacle detection
 	D1_status = D1.status
 	D2 = DD(4)	##Back DD , person detection
+
+	while(True):
+		if(D1.status == False):
+			# Obstacle has been detected
+			None
+		elif(camera.test() < 0.2):
+			# Front got no obstacle but do not dectect the face
+			None
+		elif(D2.status == True):
+			# Front got no obstacle and do detect the face but the person is out of the range
+			None
+		else:
+			# Fron got no obstacle and do detect the face and the person is in the range
+			None
+
