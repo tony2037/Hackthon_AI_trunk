5ed3fe74e83564f079c0938ea8245e71e9340aa5
tony2037 <ztex030640417@gmail.com>
2018-04-01 01:42:02 +0800

    change DD to BCM mode

--- AItrunk/DD.py
+++ AItrunk/DD.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 class DD:
     def __init__(self,pin):
         self.pin = pin
-        GPIO.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     def status(self): 
         return  GPIO.input(self.pin)
