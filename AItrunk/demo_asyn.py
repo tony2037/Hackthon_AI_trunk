76d59b2b902093857b2acba37f0491d75adf1910
張景雲 <z5254215560@gmail.com>
2018-04-01 03:23:46 +0800

    make request as asyn

---
+++ AItrunk/demo_asyn.py
@@ -0,0 +1,17 @@
+from multiprocessing import Process
+from multiprocessing import Pool
+import os ,time
+
+def aaa():
+    time.sleep(1)
+    for i in range(3):
+        print("aaa")
+def bbb():
+    for i in range(3):
+        print("bbb")
+if __name__=='__main__':
+    p = Pool()
+    p.apply_async(aaa)
+    p.apply_async(bbb)
+    p.close()
+    p.join()
--- AItrunk/server.py
+++ AItrunk/server.py
@@ -3,32 +3,31 @@ from modules.camera import Camera
 from modules.controller import Controller
 from modules.DD import DD
 import time
+from multiprocessing import Process
+import os
+def request_img():
+    camera = Camera()
+    controller = Controller()
+    D1 = DD(27)
+    D2 = DD(4)
+    while(1):
+        if(camera.face_detection() > 0.2 and D2==False and D1==True):
+            controller.Forward()
 
+def detect_distance():
+    controller = Controller()
+    D1 = DD(27)
+    D2 = DD(4)
+    while(1):
+        if(D1.status() == False):
+            controller.Stop()
+        elif(D2.status() == True):
+            controller.Stop()
 
 
 
 
 if __name__ == '__main__':
-	camera = Camera()
-	controller = Controller()
-	D1 = DD(27)	#Front DD , obstacle detection
-	D2 = DD(4)	##Back DD , person detection
-	while(True):
-            c = camera.test()
-            print("The confidence : " + str(c))
-            if(D1.status() == False):
-                    # Obstacle has been detected
-                    # stop
-                    controller.Stop()
-            elif(c < 0.2 and D1.status() == True):
-                    # Front got no obstacle but do not dectect the face
-                    # stop
-                    controller.Stop()
-            elif(c > 0.2 and D2.status() == True):
-                    # Front got no obstacle and do detect the face but the person is out of the range
-                    # stop
-                    controller.Stop()
-            else:
-                    # Fron got no obstacle and do detect the face and the person is in the range
-                    controller.Forward()
-
+    p = Pool()
+    p.apply_async(request_img)
+    p.apply_async(detect_distance)
