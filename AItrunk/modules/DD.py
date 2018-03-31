39a8ca752c12ceb466fd7552906c5b8e884c11e4
tony2037 <ztex030640417@gmail.com>
2018-04-01 02:11:31 +0800

    detail about the DD status

--- AItrunk/modules/DD.py
+++ AItrunk/modules/DD.py
@@ -6,6 +6,8 @@ class DD:
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     def status(self): 
         return  GPIO.input(self.pin)
+        # true : Has not detection
+        # false : Has detection
     def callback(self,my_callback):
         GPIO.add_event_detect(self.pin, GPIO.RISING)
         GPIO.add_event_callback(self.pin, my_callback)
